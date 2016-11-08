22154a903b3f2021817a97742ec5716751759edc
Vladimir Ondrus <vladimir.ondrus@gmail.com>
2016-11-08 23:12:49 +0100

    Update main.go

--- main.go
+++ main.go
@@ -2,10 +2,11 @@ package main // import "github.com/hashicorp/vault"
 
 import (
 	"os"
-
+	"fmt"
 	"github.com/hashicorp/vault/cli"
 )
 
 func main() {
+	fmt.Println("Hello")
 	os.Exit(cli.Run(os.Args[1:]))
 }
